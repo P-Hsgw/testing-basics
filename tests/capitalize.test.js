8ab145615ca874824c89cc4813c1cf8338c2253a
Patryk Hasegawa <contact@p-hsgw.com>
2022-04-25 10:41:31 +0900

    Add more tests to capitalize.js function

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,5 +1,13 @@
 import capitalize from "../functions/capitalize"
 
-test('"test" should become "Test"', () => {
+test(`"test" should become "Test"`, () => {
   expect(capitalize('test')).toBe('Test');
 });
+
+test(`"0123234dsad2312" should become "0123234dsad2312"`, () => {
+  expect(capitalize(`0123234dsad2312`)).toBe(`0123234dsad2312`)
+});
+
+test(`"TeStinGs" should become "TeStinGs"`, () => {
+  expect(capitalize(`TeStinGs`)).toBe(`TeStinGs`)
+})
